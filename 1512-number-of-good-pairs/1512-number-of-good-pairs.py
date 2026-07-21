99b8f5ea948fbd5fc23ce00e61aa1898acc524fe
Ashu Kumar <ashuwebdev271@gmail.com>
2026-07-21 15:14:24 +0530

    undefined - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,6 +1,24 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        freq=[0]*101
+        for num in nums:
+            freq[num]+=1
+        for i in range(101):  
+            freq[i]=freq[i]*(freq[i]-1)//2
+
         count=0
+        for x in freq:
+            count += x
+        return count
+
+
+
+
+
+
+
+
+
 
         for i in range(len(nums)):
             for j in range(len(nums)):
