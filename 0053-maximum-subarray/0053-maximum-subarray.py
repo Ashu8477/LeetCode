bf24d1a4cbe86d2c7c06dfc8cac98eddc32e4e0e
Ashu Kumar <ashuwebdev271@gmail.com>
2026-07-24 00:15:24 +0530

    undefined - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,9 +5,7 @@ class Solution:
         for num in nums:
             curr_sum+=num
             max_sum=max(max_sum,curr_sum)
-
             if curr_sum<0:
                 curr_sum=0
-
         return max_sum
         
