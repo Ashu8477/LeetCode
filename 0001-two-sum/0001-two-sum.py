31993c85498cb4c7dfe359dbd5ea6927814f9737
Ashu Kumar <ashuwebdev271@gmail.com>
2026-07-25 21:03:48 +0530

    undefined - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic={}
-        for i in range(len(nums)):
+
+        n=len(nums)
+        hashMap={}
+
+
+        for i in range(n):
             value=target-nums[i]
-            if value in dic:
-                return dic[value],i
-                
-            else:
-                dic[nums[i]]=i
-       
+            if value in hashMap:
+                return hashMap[value],i
+            hashMap[nums[i]]=i
 
 
         
