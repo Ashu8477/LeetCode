f0ba0fda98f5f67ec1bc84d4d826fdcfc7a77208
Ashu Kumar <ashuwebdev271@gmail.com>
2026-08-23 22:55:39 +0530

    undefined - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -1,15 +1,10 @@
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        n=len(nums)
-        count=0
-        even_digit=0
-        for num in nums:
-            while num>0:
-                last_digit=num%10
-                count+=1
-                num=num//10
-            if count%2==0:
-                even_digit+=1
-            count=0
 
-        return even_digit
+        even_digits=0
+
+        for num in nums:
+
+            if 10<=num<=99 or 1000<=num<=9999 or num==100000:
+                even_digits+=1
+        return even_digits
