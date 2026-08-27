d6369e888ed063ee849f9c87abdc155eef1437a3
Ashu Kumar <ashuwebdev271@gmail.com>
2026-08-28 01:37:41 +0530

    undefined - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -3,15 +3,14 @@ class Solution:
         n=len(nums)
         prefix=0
         hash={0:-1}
-        ans=0
+        count=0
         for i in range(n):
             if nums[i]==0:
                 prefix-=1
             else:
                 prefix+=1
             if prefix in hash:
-                ans = max(ans, i - hash[prefix])
+                count=max(count,i-hash[prefix])
             else:
                 hash[prefix]=i
-        return ans
-        
+        return count
