37bc4fdf560d4e202f80e3f1f641448761f7ed7a
Ashu Kumar <ashuwebdev271@gmail.com>
2026-09-10 23:58:28 +0530

    undefined - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         count={}
-       
-
-
         for i in range(len(nums)):
             value=target-nums[i]
             if value not in count:
