7c9d8e330d6f722fc1ed46fe2e42cac7309d0a03
Ashu Kumar <ashuwebdev271@gmail.com>
2026-09-15 23:01:08 +0530

    undefined - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack=[]
+        prev=0
+        for ch in operations:
+            
+            if ch=='C':
+                stack.pop()
+            elif ch=='D':
+                stack.append(2*stack[-1])
+            elif ch=='+':
+                
+                stack.append(stack[-1]+stack[-2])
+            else:
+                stack.append(int(ch))
+        return sum(stack)
+
+
+        
