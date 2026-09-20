6cc230de2585b3950b6652ddb39f3189a8d98b82
Ashu Kumar <ashuwebdev271@gmail.com>
2026-09-20 20:20:35 +0530

    undefined - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,20 +1,22 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-
         stack=[]
         curr=''
         num=0
         for ch in s:
-            if ch.isdigit():
-                num=num*10+int(ch)
-            elif ch=='[':
+            if ch=='[':
                 stack.append((curr,num))
                 curr=''
                 num=0
             elif ch==']':
-                prev,k=stack.pop()
-                curr=prev+curr*k
+                prev,num=stack.pop()
+                curr=prev+curr*num
+                num=0
+            elif ch.isdigit():
+                num=num*10 + int(ch)
             else:
                 curr+=ch
         return curr
+
+            
         
