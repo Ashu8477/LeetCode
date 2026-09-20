c25dd652f252f4d467a22cb4d580605576468791
Ashu Kumar <ashuwebdev271@gmail.com>
2026-09-20 13:24:45 +0530

    undefined - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,11 +1,14 @@
 class Solution:
-    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack=[]
-        mp={}
-        for num in nums2:
+    def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
 
+        mp={}
+        stack=[]
+
+        for num in nums2:
             while stack and stack[-1]<num:
                 mp[stack.pop()]=num
 
             stack.append(num)
+        
         return [mp.get(num,-1) for num in nums1]
+        
