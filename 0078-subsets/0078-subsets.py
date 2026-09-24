e2f6e483c68c65e8e15dcee35f174098dac24eb9
Ashu Kumar <ashuwebdev271@gmail.com>
2026-09-24 14:37:04 +0530

    undefined - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,15 @@
 class Solution:
     def subsets(self, nums: list[int]) -> list[list[int]]:
         ans=[]
-        n=len(nums)
-        def solve(path,rem):
+
+        def solve(start,path):
             ans.append(path[:])
 
-            for i in range(len(rem)):
-                if len(path)==len(nums):
-                    return
-                solve(path + [rem[i]],rem[i+1:])
-        solve([],nums)
+            for i in range(start,len(nums)):
+                path.append(nums[i])
+                solve(i+1,path)
+                path.pop()
+        
+        solve(0,[])
         return ans
         
