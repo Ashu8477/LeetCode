a735d914157dce73af677f658f20bfd1d9c63a06
Ashu Kumar <ashuwebdev271@gmail.com>
2026-09-24 16:08:41 +0530

    undefined - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,23 +1,24 @@
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+    def combinationSum2(self, candidates: list[int], target: int) -> list[list[int]]:
+
+        ans=[]
         candidates.sort()
-        res = []
 
-        def backtrack(start, path, total):
-            if total == target:
-                res.append(path[:])
+        def solve(path,start,total):
+            if total==target:
+                ans.append(path[:])
                 return
-            if total > target:
+            if total>target:
                 return
 
-            prev = -1
-            for i in range(start, len(candidates)):
-                if candidates[i] == prev:
+            for i in range(start,len(candidates)):
+                if i>start and candidates[i]==candidates[i-1]:
                     continue
                 path.append(candidates[i])
-                backtrack(i + 1, path, total + candidates[i])
+                solve(path,i+1,total+candidates[i])
                 path.pop()
-                prev = candidates[i]
-
-        backtrack(0, [], 0)
-        return res
+            
+        solve([],0,0)
+        return ans
+            
+        
